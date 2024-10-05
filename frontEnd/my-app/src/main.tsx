305c4762e4db02033bdc122b6b8b78fc3b548ce0
gruiiiw <107224535+gruiiiw@users.noreply.github.com>
2024-10-05 14:51:31 -0400

    update frontend

--- frontEnd/my-app/src/main.tsx
+++ frontEnd/my-app/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './pages/Home.tsx'
 import App from './App.tsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+    ],
+  },
+]); 
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ frontEnd/my-app/src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+
+  return (
+      <>
+          <p>This website is under construction!</p>
+      </>
+  )
+}
+
+export default Home

1bc988f288b4bd1ec00c2c4ff7bad216b34c6c80
IshaanPoojari2294 <ip2294@nyu.edu>
2024-10-05 21:03:57 -0400

    ai reccomendations

---
+++ app/services/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+sample_preferences = {
+    "solar_energy": 0.8,
+    "wind_energy": 0.5,
+    "biodiversity": 0.7,
+    "low_carbon_footprint": 0.9,
+    "renewable_energy_investment": 1.0,
+    "fossil_fuel_divestment": 1.0
+  }
